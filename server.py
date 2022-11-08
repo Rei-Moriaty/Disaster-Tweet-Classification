562bcfafcfa6747c84c4c31cd0aa7436d1ee1c17
unknown <kartikpowar310398@gmail.com>
2022-11-08 09:53:55 +0530

    made changes for webapp

---
+++ server.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template
+from flask import request
+import pandas as pd
+import numpy as np
+import pickle
+from flask import jsonify
+import tweepy
+
+from transformation import transform
+
+app = Flask("tweet_classifier", template_folder='pages')
+output_file = './rf_n_estimator=200_depth=15.bin'
+configuration_file = './config.txt'
+
+
+with open(output_file, 'rb') as f_in:
+    (threshold, tags_set, vectorizer, model) = pickle.load(f_in)
+
+with open(configuration_file, 'r') as f_in:
+    consumer_key = f_in.readline().strip()
+    consumer_secret = f_in.readline().strip()
+    access_key = f_in.readline().strip()
+    access_secret = f_in.readline().strip()
+
+@app.route('/', methods = ['GET'])
+def home():
+    tweets = get_tweets()
+    disaster_tweets = list()
+    non_disaster_tweets = list()
+    for tweet in tweets:
+        var_class = predict({"text" : tweet.text})
+        if var_class == 1:
+            disaster_tweets.append(tweet)
+        else:
+            non_disaster_tweets.append(tweet)
+    return render_template('home.html', 
+                            disaster_tweets =  disaster_tweets, 
+                            non_disaster_tweets =  non_disaster_tweets,
+                            len_disaster = len(disaster_tweets),
+                            len_non_disaster = len(non_disaster_tweets))
+
+@app.route('/form', methods = ['GET'])
+def form():
+    return render_template('form.html')
+
+@app.route('/class', methods = ['POST'])
+def get_class():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        tweet = {"text" : request.get_json()}
+        var_class = predict(tweet)
+        result = {"class" : var_class} 
+
+        return jsonify(result)
+    else:
+        tweet = {"text" : request.form.get("text")}
+        var_class = predict(tweet)
+        
+        return render_template('predict.html', value=var_class)
+
+def predict(tweet):
+    transformed_tweet = transform(tweet, tags_set, vectorizer)
+    X = np.array(list(transformed_tweet.values()))
+    prediction = model.predict(X.reshape(1,-1))[0]
+    if prediction >= threshold:
+        return 1
+    else:
+        return 0
+
+def get_tweets():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+
+    api = tweepy.API(auth)
+    
+    tweets = []
+    for tweet in api.search_tweets(q="naturaldisaster", lang="en"):
+        tweets.append(tweet)
+    print(tweets[0])
+    return tweets
+        
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
---
+++ transformation.py
@@ -0,0 +1,75 @@
+import re
+import nltk
+from textblob import TextBlob
+import spacy
+import collections
+import numpy as np
+
+stopwords = nltk.corpus.stopwords.words("english")
+stopwords
+
+words = ["fire", "news", "disaster", "suicide bomber", "oil spill", "california wildfire"]
+
+def preprocess_text(txt, flg_stem=False, flg_lemmatize=False, stopwords=None):
+    txt = re.sub(r'[^\w\s]', '', str(txt).lower().strip())
+    
+    result_txt = txt.split()
+    
+    if stopwords is not None:
+        result_txt = [word for word in result_txt if word not in stopwords]
+    
+    if flg_stem:
+        ps = nltk.stem.PorterStemmer()
+        result_txt = [ps.stem(word) for word in result_txt]
+    
+    if flg_lemmatize:
+        lem = nltk.stem.WordNetLemmatizer()
+        result_txt = [lem.lemmatize(word) for word in result_txt]
+    
+    txt = " ".join(result_txt)
+    return txt
+
+def utils_lst_count(lst):
+    dic_counter = collections.Counter()
+    for x in lst:
+        dic_counter[x] += 1
+    dic_counter = collections.OrderedDict( 
+                     sorted(dic_counter.items(), 
+                     key=lambda x: x[1], reverse=True))
+    lst_count = [ {key:value} for key,value in dic_counter.items() ]
+    return lst_count
+
+def utils_ner_features(lst_dics_tuples, tag):
+    if len(lst_dics_tuples) > 0:
+        tag_type = []
+        for dic_tuples in lst_dics_tuples:
+            for tuple in dic_tuples:
+                type, n = tuple[1], dic_tuples[tuple]
+                tag_type = tag_type + [type]*n
+                dic_counter = collections.Counter()
+                for x in tag_type:
+                    dic_counter[x] += 1
+        return dic_counter[tag]
+    else:
+        return 0
+
+def transform(tweet, tags_set, vectorizer):
+    tweet["text"] = preprocess_text(tweet["text"])
+    tweet["word_count"] = len(str(tweet["text"]).split(" "))
+    tweet["char_count"] = sum(len(word) for word in str(tweet["text"]).split(" "))
+    tweet["sentence_count"] = len(str(tweet["text"]).split("."))
+    tweet["avg_word_length"] = tweet["char_count"] / tweet["word_count"]
+    tweet["avg_sentence_length"] =  tweet["word_count"] / tweet["sentence_count"]
+    tweet["sentiment"] = TextBlob(tweet["text"]).sentiment.polarity
+    ner = spacy.load("en_core_web_sm")
+    tweet["tags"] =  [(tag.text, tag.label_) for tag in ner(tweet["text"]).ents]
+    tweet["tags"] = utils_lst_count(tweet["tags"])
+    for feature in tags_set:
+        tweet["tags_"+feature] = utils_ner_features(tweet["tags"], feature)
+    # tweet = vectorizer.transform([tweet["text"]])
+    vector = vectorizer.transform([tweet["text"]]).todense().tolist()[0]
+    for i in range(len(words)):
+        tweet[words[i]] = vector[i]
+    del tweet['text']
+    del tweet['tags']
+    return tweet
